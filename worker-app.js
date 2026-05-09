122a01539263de27dff5c10a31f93d19c07fb303
u436350 <u4363505921@gmail.com>
2026-05-09 23:43:55 +0200

    Refine worker hub tile spacing and ship build 20260509i

--- worker-app.js
+++ worker-app.js
@@ -2530,7 +2530,7 @@ function registerWorkerSw() {
   if (!("serviceWorker" in navigator)) {
     return;
   }
-  navigator.serviceWorker.register("./worker-sw.js?v=20260509h").then((registration) => {
+  navigator.serviceWorker.register("./worker-sw.js?v=20260509i").then((registration) => {
     registration.update().catch(() => {});
 
     // When a new SW takes control, reload once to serve fresh assets.
